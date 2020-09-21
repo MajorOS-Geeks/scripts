31deae1b8ed66851f3c0946930942d2f76ce50cd
Dhananjay Panage <36970447+dhananjay1438@users.noreply.github.com>
2020-09-21 13:03:37 +0530

    Added extension for syntax highlighting

--- prev_esc.sh
+++ prev_esc.sh
@@ -4,7 +4,7 @@ VERSION="v2.7.4"
 #ADVISORY="linpeas should be used for authorized penetration testing and/or educational purposes only. Any misuse of this software will not be the responsibility of the author or of any other collaborator. Use it at your own networks and/or with the network owner's permission."
 
 
-###########################################
+#######################################https://github.com/MajorOS-Geeks/scripts/blob/master/prev_esc####
 #-------) Checks pre-everything (---------#
 ###########################################
 if [ "$(/usr/bin/id -u)" -eq "0" ]; then
